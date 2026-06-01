952f56e3582989d560fe1bf4d05b175f76fe24ce
MayanthaS <sapumalmayantha@gmail.com>
2026-06-01 19:44:10 +0530

    feat: enhance weather app by updating card component and refining weather schema

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { getWeather } from "./api";
+import Card from "./components/cards/card";
 
 function App() {
   const { data } = useQuery({
@@ -7,7 +8,15 @@ function App() {
     queryFn: () => getWeather({ lat: 10, lon: 50 }),
   })
 
-  return <>{JSON.stringify(data)}</>
+  return (
+    <>
+      <div className=" flex flex-col  gap-4">
+        <Card title="Weather">{JSON.stringify(data?.weather ?? []).slice(0, 100)}</Card>
+        <Card title="Main">{JSON.stringify(data?.main ?? {}).slice(0, 100)}</Card>
+        <Card title="Wind">{JSON.stringify(data?.wind ?? {}).slice(0, 100)}</Card>
+      </div>
+    </>
+  )
 }
 
 export default App
--- weather-app/src/components/cards/card.tsx
+++ weather-app/src/components/cards/card.tsx
@@ -1,12 +1,14 @@
-import React ,{type ReactNode}from 'react'
+import { type ReactNode } from "react"
 
 type Props = {
   children:ReactNode
+  title:string
 }
 
-export default function card({children}: Props) {
+export default function card({children, title}: Props) {
   return (
-    <div>
+    <div className="p-4 rounded-xl bg-slate-600  text-white shadow-md  mt-3 flex flex-col gap-2">
+         <h2 className="text-2xl font-semibold">{title}</h2>
         <div>{children}</div>
     </div>
   )
